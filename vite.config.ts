cb7f76c0dacad2ef5dc0e5140249603a472a520d
Diovane <diovane@pop-os.localdomain>
2023-01-09 17:18:31 -0300

    deletado: arquivo MARKDOWN.MD(usado para definir objetivos apos a conclusao do pojeto base) removido.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<REPO>/',
+  base: '/ignite-feed-fundamentos-react/',
   plugins: [react()],
 })
